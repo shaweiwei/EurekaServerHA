3f21d82a8b57a9a67d183b93ead7e02892c7fca1
shaweiwei <shamo89@126.com>
2017-12-12 20:24:16 +0800

    1

---
+++ ServiceHello/src/main/java/com/eurekaha/BootApplication.java
@@ -0,0 +1,24 @@
+package com.eurekaha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class BootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+	}
+	
+	@RequestMapping(value="/hi",method=RequestMethod.GET)
+	public String hi(){
+		return "hi";
+	}
+
+}
